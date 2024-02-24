5fbdd68274fa71c7b8e1849c469a5afb79923d75
matheusmantini <engmatheusmantini@gmail.com>
2024-02-24 11:24:52 -0300

    Add estilos de responsividade de desktop para a destination

--- src/pages/destination.jsx
+++ src/pages/destination.jsx
@@ -26,60 +26,68 @@ const Destination = () => {
         <p className={styles.Title}>
           <span>01</span>Pick your destination
         </p>
-        <div className={styles.ImageContainer}>
-          <Image
-            src={`/${selectedData?.images?.png}`}
-            alt={selectedData?.name}
-            width={259}
-            height={259}
-          />
-        </div>
-        <div className={styles.TabContainer}>
-          <ul className={styles.Tab}>
-            <li
-              className={selectedDestination === "Moon" ? styles.Selected : ""}
-              onClick={() => handleSelectTab("Moon")}
-            >
-              Moon
-            </li>
-            <li
-              className={selectedDestination === "Mars" ? styles.Selected : ""}
-              onClick={() => handleSelectTab("Mars")}
-            >
-              Mars
-            </li>
-            <li
-              className={
-                selectedDestination === "Europa" ? styles.Selected : ""
-              }
-              onClick={() => handleSelectTab("Europa")}
-            >
-              Europa
-            </li>
-            <li
-              className={selectedDestination === "Titan" ? styles.Selected : ""}
-              onClick={() => handleSelectTab("Titan")}
-            >
-              Titan
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h2 className={styles.Name}>{selectedData?.name}</h2>
-          <div className={styles.DescriptionContainer}>
-            <p className={styles.Description}>{selectedData?.description}</p>
+        <div className={styles.ContentWrapper}>
+          <div className={styles.ImageContainer}>
+            <Image
+              src={`/${selectedData?.images?.png}`}
+              alt={selectedData?.name}
+              width={259}
+              height={259}
+            />
           </div>
-          <div className={styles.Details}>
-            <p className={styles.Detail}>
-              <span className={styles.DetailTitle}>AVG. DISTANCE </span>
-              <span className={styles.DetailInfo}>
-                {selectedData?.distance}
-              </span>
-            </p>
-            <p className={styles.Detail}>
-              <span className={styles.DetailTitle}>Est. travel time </span>
-              <span className={styles.DetailInfo}>{selectedData?.travel}</span>
-            </p>
+          <div className={styles.TabContainer}>
+            <ul className={styles.Tab}>
+              <li
+                className={
+                  selectedDestination === "Moon" ? styles.Selected : ""
+                }
+                onClick={() => handleSelectTab("Moon")}
+              >
+                Moon
+              </li>
+              <li
+                className={
+                  selectedDestination === "Mars" ? styles.Selected : ""
+                }
+                onClick={() => handleSelectTab("Mars")}
+              >
+                Mars
+              </li>
+              <li
+                className={
+                  selectedDestination === "Europa" ? styles.Selected : ""
+                }
+                onClick={() => handleSelectTab("Europa")}
+              >
+                Europa
+              </li>
+              <li
+                className={
+                  selectedDestination === "Titan" ? styles.Selected : ""
+                }
+                onClick={() => handleSelectTab("Titan")}
+              >
+                Titan
+              </li>
+            </ul>
+            <h2 className={styles.Name}>{selectedData?.name}</h2>
+            <div className={styles.DescriptionContainer}>
+              <p className={styles.Description}>{selectedData?.description}</p>
+            </div>
+            <div className={styles.Details}>
+              <p className={styles.Detail}>
+                <span className={styles.DetailTitle}>AVG. DISTANCE </span>
+                <span className={styles.DetailInfo}>
+                  {selectedData?.distance}
+                </span>
+              </p>
+              <p className={styles.Detail}>
+                <span className={styles.DetailTitle}>Est. travel time </span>
+                <span className={styles.DetailInfo}>
+                  {selectedData?.travel}
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
